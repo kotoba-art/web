bef472f8d2874b43dfc5d7a59e09bd40cec20b0c
Daniel Ishigaki <danielvichi@gmail.com>
2021-05-24 14:19:19 -0300

    Add Footer and fix header on some mobile views

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMcontenLoaded", function() {
+
+  document.addEventListener("scroll", function(event){
+    console.log(event)
+  })  
+
+});
+
+
+document.addEventListener("keydown", function(event){
+    console.log(event)
+  })  
